4f387144ca73ba9ac5ec4bfa6dfa9432fed1a491
ikuzwe alain yves prince <alainprinceikuzwe@gmail.com>
2025-09-13 02:13:25 +0200

    Fix CORS and link frontend

--- server.js
+++ server.js
@@ -27,3 +27,7 @@ app.use('/api/experts', expertRoutes);
 app.get('/', (req, res) => res.send('AgriConnect API is running'));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.use(cors({
+  origin: process.env.CLIENT_URL || '*'
+}));
